1df063db72951dafb7e7c39a4d64ffdb42a020cb
elawrance <elawrance@outlook.com>
2017-07-24 02:20:37 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The Assignment to make Cache matrix and check
+## it for cache
 
-## Write a short comment describing this function
+## Make a Cache matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mi = NULL
+    set = function(n){
+        m <<- n
+        mi <<- NULL
+    }
+    get <- function() m
+    setinv <- function(mean) mi <<- solve(m)
+    getinv <- function() mi
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## To check for the Cache and populate it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    mi <- m$getinv()
+    if(!is.null(mi)) {
+        message("getting cached data")
+        return(mi)
+    }
+    data <- m$get()
+    mi <- solve(data, ...)
+    m$setinv(mi)
+    mi
 }
